c2b506bb45e68067a297898de3317aafc80c6f8b
OmarThierno <thiernomarndiaye76@gmail.com>
2024-08-11 12:36:11 +0200

    feat: I connected to the DB, I checked in case there was no connection

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashbord</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php 
+
+define("DB_SERVERNAME", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "universal_db_small");
+define("DB_PORT", 8889);
+
+
+try {
+  $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+} catch (Exception $e) {
+  echo $e->getMessage();
+  die();
+}
+
+var_dump($connection);
