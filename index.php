06edd87bf591e41835c28df88407548383e5ac4f
OmarThierno <thiernomarndiaye76@gmail.com>
2024-08-11 14:22:01 +0200

    feat: created the departments view in a table

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once __DIR__ . "/Models/department.php";
 
 define("DB_SERVERNAME", "localhost");
@@ -23,11 +23,11 @@ $result = $connection->query($sql);
 
 $departments = [];
 
-if($result && $result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
   $row = $result->fetch_object();
   // var_dump($row);
 
-  while($row) {
+  while ($row) {
     $newDepartment = new Department($row->id, $row->name);
     $newDepartment->setAddress($row->address);
     $newDepartment->setPhone($row->phone);
@@ -42,4 +42,77 @@ if($result && $result->num_rows > 0) {
   }
 }
 
-var_dump($departments);
+// var_dump($departments);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <!-- NavBar  -->
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Dashboard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- /NavBar  -->
+
+  <div class="container">
+    <h2>Lista di Dipartimenti</h2>
+    <?php if (count($departments) > 0) { ?>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">name</th>
+            <th scope="col">address</th>
+            <!-- <th scope="col">phone</th>
+            <th scope="col">email</th>
+            <th scope="col">website</th> -->
+            <th scope="col">head of department</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($departments as $department) { ?>
+          <tr>
+            <th scope="row"><?php echo $department->getName() ?></th>
+            <td><?= $department->getAddress() ?></td>
+            <!-- <td><?= $department->getPhone() ?></td>
+            <td><?= $department->getEmail() ?></td>
+            <td><?= $department->getWebsite() ?></td> -->
+            <td><?= $department->getHeadOfDepartment() ?></td>
+            <td></td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    <?php } else { ?>
+      <h3>Non ci sono risultati</h3>
+    <?php } ?>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
