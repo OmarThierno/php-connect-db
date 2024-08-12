7bc3e0c63357e23259197390e956d87da2c44680
OmarThierno <thiernomarndiaye76@gmail.com>
2024-08-12 11:42:32 +0200

    feat: added the login form and protected queries from injection

--- index.php
+++ index.php
@@ -6,6 +6,25 @@ require_once __DIR__ . "/helpers/departments-funcion.php";
 // Start connection with database 
 $connection = startConnection();
 
+// check your login
+if(isset($_POST["username"]) && isset($_POST["password"])) {
+  $usename = $_POST["username"];
+  $password = $_POST["password"];
+
+  $hashedPassword = md5($password);
+
+  // risk of SQL injection
+  // $sql = "SELECT * FROM `users` WHERE `username` = '$usename' AND `password` = '$hashedPassword';";
+  // $result = $connection->query($sql);
+
+  $statement = $connection->prepare("SELECT * FROM `users` WHERE `username` = ? AND `password` = ? ;");
+  $statement->bind_param("ss", $usename, $hashedPassword);
+  $statement->execute();
+  $result = $statement->get_result();
+  
+  var_dump($result);
+}
+
 // I pick up all departments
 $departments = getAllDepartments($connection);
 
---
+++ partials/login.php
@@ -0,0 +1,21 @@
+<h1 class="text-center">Login</h1>
+<div class="row justify-content-center">
+  <div class="col-6">
+    <div class="card">
+      <div class="card-body">
+        <form action="index.php" method="POST">
+          <div class="mb-3">
+            <label class="form-label" for="username">Username</label>
+            <input class="form-control" type="text" id="username" name="username">
+          </div>
+          <div class="mb-3">
+            <label class="form-label" for="password">Password</label>
+            <input class="form-control" type="password" id="password" name="password">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
